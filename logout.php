7964671931318202127a9098671d6890782bc7d0
kyleyiyang <kyleyiyang@gmail.com>
2015-06-27 22:29:57 -0400

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'header.php';
+echo "<h3>Log out</h3>";
+
+if (isset($_SESSION['user'])) {
+    destroySession();
+    echo "You have been logged out. Please
+    <a href='index.php'>click here</a> to refresh the screen.";
+} else echo "You are not logged in";
+?>
