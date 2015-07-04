1fa16a15ad9c9b1439936d1ae36f03efe9ba7ea1
kyleyiyang <kyleyiyang@gmail.com>
2015-07-03 23:38:34 -0400

    Update header.php

--- header.php
+++ header.php
@@ -9,16 +9,17 @@ if (isset($_SESSION['user'])) {
 
 echo "<html><head><title>$appname";
 if ($loggedin) echo " ($user)";
-echo "</title></head><style>body {width: 60%; margin: 0 auto}</style><body><font face='verdana' size='2'>";
+echo "</title></head><style>body {width: 54%; margin: 0 auto} a {font-size: 14px}</style><body><font face='verdana' size='2'>";
 echo "<h2>$appname</h2>";
 
 if ($loggedin) {
-    echo "<b>Welcome $user</b>:
+    echo "<b>Welcome $user!</b><br />
         <a href='members.php?view=$user'>Home</a> |
         <a href='members.php'>Members</a> |
         <a href='friends.php'>Friends</a> |
         <a href='messages.php'>Messages</a> |
         <a href='profile.php'>Profile</a> |
+        <a href='search.php'>Search Members</a> |
         <a href='logout.php'>Log out</a> |";
 } else {
     echo "<a href='index.php'>Home</a> | 
